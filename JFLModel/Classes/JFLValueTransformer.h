5159e67d21bc9fb92bbf7437bee91534d6fb0dd0
LiJunfeng <jfl913@163.com>
2016-11-02 20:02:42 +0800

    no message

--- JFLModel/Classes/JFLValueTransformer.h
+++ JFLModel/Classes/JFLValueTransformer.h
@@ -7,12 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JFLTransformerErrorHandling.h"
 
 // TODO: 为什么success和error这样写？
 typedef id(^JFLValueTransformerBlock)(id value, BOOL *success, NSError **error);
 
-@interface JFLValueTransformer : NSValueTransformer
+@interface JFLValueTransformer : NSValueTransformer <JFLTransformerErrorHandling>
 
-//+ (instancetype)
++ (instancetype)transformerUsingForwardBlock:(JFLValueTransformerBlock)transformation;
+
++ (instancetype)transformerUsingReversibleBlock:(JFLValueTransformerBlock)transformation;
+
++ (instancetype)transformerUsingForwardBlock:(JFLValueTransformerBlock)forwardTransformation reverseBlock:(JFLValueTransformerBlock)reverseTransformation;
 
 @end
--- JFLModel/Classes/JFLValueTransformer.m
+++ JFLModel/Classes/JFLValueTransformer.m
@@ -8,6 +8,119 @@
 
 #import "JFLValueTransformer.h"
 
+@interface JFLReversibleValueTransformer : JFLValueTransformer
+@end
+
+@interface JFLValueTransformer ()
+
+@property (nonatomic, copy, readonly) JFLValueTransformerBlock forwardBlock;
+@property (nonatomic, copy, readonly) JFLValueTransformerBlock reverseBlock;
+
+@end
+
 @implementation JFLValueTransformer
+
+#pragma mark Liftcycle
+
++ (instancetype)transformerUsingForwardBlock:(JFLValueTransformerBlock)forwardBlock
+{
+    return [[self alloc] initWithForwardBlock:forwardBlock reverseBlock:nil];
+}
+
++ (instancetype)transformerUsingReversibleBlock:(JFLValueTransformerBlock)reversibleBlock
+{
+    return [self transformerUsingForwardBlock:reversibleBlock reverseBlock:reversibleBlock];
+}
+
++ (instancetype)transformerUsingForwardBlock:(JFLValueTransformerBlock)forwardBlock reverseBlock:(JFLValueTransformerBlock)reverseBlock
+{
+    return [[JFLReversibleValueTransformer alloc] initWithForwardBlock:forwardBlock reverseBlock:reverseBlock];
+}
+
+- (id)initWithForwardBlock:(JFLValueTransformerBlock)forwardBlock reverseBlock:(JFLValueTransformerBlock)reverseBlock
+{
+    NSParameterAssert(forwardBlock != nil);
+    
+    self = [super init];
+    if (self == nil) return nil;
+    
+    _forwardBlock = [forwardBlock copy];
+    _reverseBlock = [reverseBlock copy];
+    
+    return self;
+}
+
+#pragma mark NSValueTransformer
+
++ (BOOL)allowsReverseTransformation
+{
+    return NO;
+}
+
++ (Class)transformedValueClass
+{
+    return [NSObject class];
+}
+
+- (id)transformedValue:(id)value
+{
+    NSError *error = nil;
+    BOOL success = YES;
+    
+    return self.forwardBlock(value, &success, &error);
+}
+
+- (id)transformedValue:(id)value success:(BOOL *)outerSuccess error:(NSError *__autoreleasing *)outerError
+{
+    NSError *error = nil;
+    BOOL success = YES;
+    
+    id transformedValue = self.forwardBlock(value, &success, &error);
+    
+    if (outerSuccess != NULL) *outerSuccess = success;
+    if (outerError != NULL) *outerError = error;
+    
+    return transformedValue;
+}
+
+@end
+
+@implementation JFLReversibleValueTransformer
+
+#pragma mark Liftcycle
+
+- (id)initWithForwardBlock:(JFLValueTransformerBlock)forwardBlock reverseBlock:(JFLValueTransformerBlock)reverseBlock
+{
+    NSParameterAssert(reverseBlock != nil);
+    return [super initWithForwardBlock:forwardBlock reverseBlock:reverseBlock];
+}
+
+#pragma mark NSValueTransformer
+
++ (BOOL)allowsReverseTransformation
+{
+    return YES;
+}
+
+- (id)reverseTransformedValue:(id)value
+{
+    NSError *error = nil;
+    BOOL success = YES;
+    
+    return self.reverseBlock(value, &success, &error);
+}
+
+- (id)reverseTransformedValue:(id)value success:(BOOL *)outerSuccess error:(NSError *__autoreleasing *)outerError
+{
+    NSError *error = nil;
+    BOOL success = YES;
+    
+    id transformedValue = self.reverseBlock(value, &success, &error);
+    
+    if (outerSuccess != NULL) *outerSuccess = success;
+    if (outerError != NULL) *outerError = error;
+    
+    return transformedValue;
+}
 
 @end
